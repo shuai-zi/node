1dbbd525f3409dabc1e0c472236df3899e512066
Shuaizi <491717309@qq.com>
2017-12-04 12:55:00 +0800

    给图片上水印demo
    给图片上水印demo

---
+++ node-demo-img-water/app.js
@@ -0,0 +1,18 @@
+var fs = require('fs')
+//var imagesInfo = require('imagesinfo')
+var images = require('images');
+var path = require('path');
+var watermarkImg = images('water_logo.png');
+var img = images('./img/afajr_r0.png');
+
+//放置右下角
+var W = img.width();
+var H = img.height();
+var mW = watermarkImg.width();
+var mH = watermarkImg.height();
+
+images(img)
+//设置绘制的坐标位置，右下角距离 40px
+.draw(watermarkImg,W - mW - 10,H - mH - 10)
+//保存
+.save('./saveImg/saveimg.png');
